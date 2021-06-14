745bbed0cd5e4c76c3c1822804c8e47673f6de7a
Karsten Bolding <karsten@bolding-bruggeman.com>
2021-06-14 15:00:45 +0200

    use pygetm/parallel.py

--- pygetm/__init__.py
+++ pygetm/__init__.py
@@ -1,38 +1,41 @@
+import numpy
+
 from . import _pygetm, core, domain
 
 Advection = _pygetm.Advection
 
-class Simulation(_pygetm.Simulation, core.FortranObject):
-    def __init__(self, domain, runtype: int, advection_scheme: int=4, apply_bottom_friction: bool=True):
-        assert not domain.initialized
-        self.domain = domain
-        _pygetm.Simulation.__init__(self, domain, runtype, advection_scheme, apply_bottom_friction)
+class Simulation(_pygetm.Simulation):
+    def __init__(self, dom: domain.Domain, runtype: int, advection_scheme: int=4, apply_bottom_friction: bool=True):
+        assert not dom.initialized
+        _pygetm.Simulation.__init__(self, dom, runtype, advection_scheme, apply_bottom_friction)
 
-        self.dist_zT = None
-        if domain.tiling:
-            self.dist_zT = self.domain.distribute(self.domain.T.z_)
-            self.dist_zU = self.domain.distribute(self.domain.U.z_)
-            self.dist_zV = self.domain.distribute(self.domain.V.z_)
-            self.dist_zX = self.domain.distribute(self.domain.X.z_)
-            self.update_depth()
+        self.update_depth()
 
-        self.get_arrays(('U', 'V'), source=0, halo=self.domain.halo)
-        self.get_arrays(('dpdx', 'dpdy'), source=1, halo=self.domain.halo)
+        for name in ('U', 'V', 'fU', 'fV', 'advU', 'advV'):
+            setattr(self, name, self.wrap(core.Array(), name.encode('ascii'), source=1))
+        for name in ('dpdx', 'dpdy'):
+            setattr(self, name, self.wrap(core.Array(), name.encode('ascii'), source=2))
 
     def update_depth(self):
         # Halo exchange for sea level on T grid
-        if self.dist_zT:
-            self.dist_zT.update_halos()
+        self.domain.T.z.update_halos()
 
         # Compute sea level on U, V, X grids
         self.update_sealevel_uvx()
 
-        if self.dist_zT:
-            # Halo exchange for sea level on U, V, X grids
-            self.dist_zU.update_halos()
-            self.dist_zV.update_halos()
-            self.dist_zX.update_halos()
+        # Halo exchange for sea level on U, V, X grids
+        self.domain.U.z.update_halos()
+        self.domain.V.z.update_halos()
+        self.domain.X.z.update_halos()
 
         # Update total water depth D on T, U, V, X grids
         # This also processes the halos; no further halo exchange needed.
         self.domain.update_depths()
+
+    @property
+    def Ekin(self, rho0: float=1025.):
+        dom = self.domain
+        U = self.U.interp(dom.T)
+        V = self.V.interp(dom.T)
+        vel2_D2 = U**2 + V**2
+        return 0.5 * rho0 * dom.T.area * vel2_D2 / dom.T.D
--- pygetm/domain.py
+++ pygetm/domain.py
@@ -20,27 +20,27 @@ def find_interfaces(c: numpy.ndarray):
     c_if[-1] = c[-1] + 0.5 * d[-1]
     return c_if
 
-class Grid(_pygetm.Grid, core.FortranObject):
-    def __init__(self, domain: 'Domain', grid_type: bytes, ioffset: int, joffset: int):
-        _pygetm.Grid.__init__(self, domain, ord(grid_type))
+class Grid(_pygetm.Grid):
+    def __init__(self, domain: 'Domain', grid_type: int, ioffset: int, joffset: int):
+        _pygetm.Grid.__init__(self, domain, grid_type)
         self.halo = domain.halo
-        self.domain = domain
         self.type = grid_type
         self.ioffset = ioffset
         self.joffset = joffset
 
     def initialize(self):
-        self.get_arrays(('x', 'y', 'dx', 'dy', 'lon', 'lat', 'dlon', 'dlat', 'H', 'D', 'mask', 'z', 'zo', 'area', 'iarea', 'cor'), dtypes={'mask': int}, halo=self.halo)
+        for name in ('x', 'y', 'dx', 'dy', 'lon', 'lat', 'dlon', 'dlat', 'H', 'D', 'mask', 'z', 'zo', 'area', 'iarea', 'cor'):
+            setattr(self, name, self.wrap(core.Array(), name.encode('ascii')))
         self.fill()
 
     def fill(self):
         read_only = ('dx', 'dy', 'lon', 'lat', 'x', 'y', 'cor', 'area')
-        nj, ni = self.H_.shape
+        nj, ni = self.H.all_values.shape
         has_bounds = self.ioffset > 0 and self.joffset > 0 and self.domain.H_.shape[-1] >= self.ioffset + 2 * ni and self.domain.H_.shape[-2] >= self.joffset + 2 * nj
         for name in ('H', 'mask', 'dx', 'dy', 'lon', 'lat', 'x', 'y', 'cor', 'area', 'z', 'zo'):
             source = getattr(self.domain, name + '_')
             if source is not None:
-                target = getattr(self, name + '_')
+                target = getattr(self, name).all_values
                 target[:, :] = source[self.joffset:self.joffset + 2 * nj:2, self.ioffset:self.ioffset + 2 * ni:2]
                 target.flags.writeable = name not in read_only
                 if has_bounds:
@@ -49,13 +49,10 @@ class Grid(_pygetm.Grid, core.FortranObject):
                     values_i = source[self.joffset - 1:self.joffset + 2 * nj + 1:2, self.ioffset - 1:self.ioffset + 2 * ni + 1:2]
                     setattr(self, name + 'i_', values_i)
                     setattr(self, name + 'i', values_i[self.halo:-self.halo, self.halo:-self.halo])
-        self.iarea_[:, :] = 1. / self.area_[:, :]
+        self.iarea.all_values[:, :] = 1. / self.area.all_values[:, :]
 
-    def array(self, fill=None, dtype=float):
-        data = numpy.empty(self.H_.shape, dtype=dtype)
-        if fill is not None:
-            data[...] = fill
-        return data[self.halo:-self.halo, self.halo:-self.halo], data
+    def array(self, fill=None, dtype=float) -> core.Array:
+        return core.Array.create(self, fill, dtype)
 
     def as_xarray(self, data):
         assert data.shape == self.x.shape
@@ -82,6 +79,20 @@ class Grid(_pygetm.Grid, core.FortranObject):
         save('mask')
         save('area', 'm2')
         save('cor', 's-1', 'Coriolis parameter')
+
+    def map(self, source, source_grid: 'Grid'):
+        assert isinstance(source_grid, Grid)
+        target = None
+        if self.type == _pygetm.TGRID:
+            if source_grid.type == _pygetm.UGRID:
+                target = numpy.ma.masked_all(source.shape)
+                target[:, 1:] = 0.5 * (source[:, :-1] + source[:, 1:])
+            elif source_grid.type == _pygetm.VGRID:
+                target = numpy.ma.masked_all(source.shape)
+                target[1:, :] = 0.5 * (source[:-1, :] + source[1:, :])
+        if target is None:
+            raise NotImplementedError('Map not implemented for grid type %i -> grid type %i' % (source_grid.type, self.type))
+        return target
 
 def read_centers_to_supergrid(ncvar, ioffset: int, joffset: int, nx: int, ny: int, dtype=None):
     if dtype is None:
@@ -297,10 +308,10 @@ class Domain(_pygetm.Domain):
         self.shape = (nz, ny + 2 * self.halo, nx + 2 * self.halo)
 
         # Create grids
-        self.T = Grid(self, b'T', ioffset=1, joffset=1)
-        self.U = Grid(self, b'U', ioffset=2, joffset=1)
-        self.V = Grid(self, b'V', ioffset=1, joffset=2)
-        self.X = Grid(self, b'X', ioffset=0, joffset=0)
+        self.T = Grid(self, _pygetm.TGRID, ioffset=1, joffset=1)
+        self.U = Grid(self, _pygetm.UGRID, ioffset=2, joffset=1)
+        self.V = Grid(self, _pygetm.VGRID, ioffset=1, joffset=2)
+        self.X = Grid(self, _pygetm.XGRID, ioffset=0, joffset=0)
 
         self.initialized = False
         self.open_boundaries = {}
@@ -315,7 +326,7 @@ class Domain(_pygetm.Domain):
         self.mask_[2:-2:2, 1::2][numpy.logical_and(tmask[1:, :] == 0, tmask[:-1, :] == 0)] = 0
         self.mask_[1::2, 2:-2:2][numpy.logical_and(tmask[:, 1:] == 0, tmask[:, :-1] == 0)] = 0
         self.mask_[2:-2:2, 2:-2:2][numpy.logical_and(numpy.logical_and(tmask[1:, 1:] == 0, tmask[:-1, 1:] == 0), numpy.logical_and(tmask[1:, :-1] == 0, tmask[:-1, :-1] == 0))] = 0
-        self.exchange_metric(self.mask_)
+        self.exchange_metric(self.mask_, fill_value=0)
 
         nbdyp = 0
         bdy_i, bdy_j = [], []
@@ -351,7 +362,7 @@ class Domain(_pygetm.Domain):
         mask_[2:-2:2, 1::2][numpy.logical_or(tmask[1:, :] == 0, tmask[:-1, :] == 0)] = 0
         mask_[1::2, 2:-2:2][numpy.logical_or(tmask[:, 1:] == 0, tmask[:, :-1] == 0)] = 0
         mask_[2:-2:2, 2:-2:2][numpy.logical_or(numpy.logical_or(tmask[1:, 1:] == 0, tmask[:-1, 1:] == 0), numpy.logical_or(tmask[1:, :-1] == 0, tmask[:-1, :-1] == 0))] = 0
-        self.exchange_metric(mask_)
+        self.exchange_metric(mask_, fill_value=0)
         self.mask_[...] = mask_
 
         for grid in (self.T, self.U, self.V, self.X):
