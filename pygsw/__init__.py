2d8f529a017dc807253421ac2e9f469bc1cc749c
Karsten Bolding <karsten@bolding-bruggeman.com>
2021-09-21 11:19:30 +0200

    added pygsw, _pygotm.so and updated otps and _pygetm.so

---
+++ pygsw/__init__.py
@@ -0,0 +1,1 @@
+from ._pygsw import *
---
+++ pygsw/__main__.py
@@ -0,0 +1,8 @@
+import pygsw
+
+if __name__ == '__main__':
+    locations = ([0., 0., -5000, 10, 30],)
+    for long, lat, z, t, sp in locations:
+        pt = pygsw.calculate_pt(long, lat, z, t, sp)
+        print('potential temperature(lon=%s, lat=%s, z=%s, t=%s, sp=%s) = %s' % (long, lat, z, t, sp, pt))
+
